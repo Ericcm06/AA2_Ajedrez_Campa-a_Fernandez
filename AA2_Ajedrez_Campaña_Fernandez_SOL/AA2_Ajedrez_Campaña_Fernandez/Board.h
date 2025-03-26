67316d4e70ea5d1b6878ea1944a58da38135ca55
Ericcm06 <eric.campana@enti.cat>
2025-03-26 09:32:05 +0100

    Colocacion de piezas y organizado en funciones.

--- AA2_Ajedrez_Campaña_Fernandez_SOL/AA2_Ajedrez_Campaña_Fernandez/Board.h
+++ AA2_Ajedrez_Campaña_Fernandez_SOL/AA2_Ajedrez_Campaña_Fernandez/Board.h
@@ -1,3 +1,6 @@
 #pragma once
 #include "const.h"
 
+void initChessBoard(char chessBoard[BOARD_SIZE][BOARD_SIZE]);
+
+void viewChessBoard(char chessBoard[BOARD_SIZE][BOARD_SIZE]);
--- AA2_Ajedrez_Campaña_Fernandez_SOL/AA2_Ajedrez_Campaña_Fernandez/main.cpp
+++ AA2_Ajedrez_Campaña_Fernandez_SOL/AA2_Ajedrez_Campaña_Fernandez/main.cpp
@@ -1,33 +1,15 @@
 #include "const.h"
+#include "board.h"
 
 int main() {
-    char chestBoard[BOARD_SIZE][BOARD_SIZE];
+    char chessBoard[BOARD_SIZE][BOARD_SIZE];
 
     std::cout << "¡¡¡Bienvenido al mejor ajedrez!!!" << std::endl;
 
-    // Inicializar el tablero
-    for (int i = 0; i < BOARD_SIZE; i++) {
-        for (int j = 0; j < BOARD_SIZE; j++) {
-            chestBoard[i][j] = CHAR_BG_BOARD;
-        }
-    }
-
-    // Mostrar índices de las columnas
-    std::cout << ' ' << ' ';
-    for (int i = 0; i < BOARD_SIZE; i++) {
-        std::cout << i + 1 << ' ';
-    }
-    std::cout << std::endl;
-
-    // Imprimir el tablero
-    for (int i = 0; i < BOARD_SIZE; i++) {
-        std::cout << BOARD_SIZE - i << ' ';
-
-        for (int j = 0; j < BOARD_SIZE; j++) {  
-            std::cout << chestBoard[i][j] << ' ';
-        }
-        std::cout << std::endl;
-    }
+    
+    initChessBoard(chessBoard); // Inicializar el tablero
+    viewChessBoard(chessBoard); // Imprimir el tablero
+    
 
     return 0;
 }
